2d32e9f7dda7e325330b04ee609df9d435e31d28
chulho you <zorroray@naver.com>
2017-06-01 16:00:00 +0900

    서비스워커 수정본

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,4 +1,4 @@
-const PRECACHE_NAME = 'precache-v2';   // 캐시명
+const PRECACHE_NAME = 'precache-v1';   // 캐시명
 const RUNTIME_NAME = 'runtime';
 // 캐시지정 파일목록
 const PRECACHE_URLS = [
@@ -97,7 +97,6 @@ this.addEventListener('fetch', function(event) {
 });
 */
 
-/*
 //-----------------------------------------
 // 서비스워커 푸시발생(push)
 //-----------------------------------------
@@ -133,4 +132,3 @@ self.addEventListener('notificationclick', function(event) {
         })
     );
 });
-*/
